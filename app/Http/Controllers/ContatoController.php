e06e1f1597a37482629a5f1ea8af4a6c8532ecac
Daniel Lopes <daniel.lopes.amorim@hotmail.com>
2022-04-21 17:14:23 -0300

    Adicionando controller contato e model contato

---
+++ app/Http/Controllers/ContatoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Controle;
+use Illuminate\Http\Request;
+
+class ContatoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Controle  $controle
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Controle $controle)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Controle  $controle
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Controle $controle)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Controle  $controle
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Controle $controle)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Controle  $controle
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Controle $controle)
+    {
+        //
+    }
+}
